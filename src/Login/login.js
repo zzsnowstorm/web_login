441f9dbc114aaa09286f5e87eeb393516f797aec
李圣安 <lsa1669778762@163.com>
2018-09-05 09:31:22 +0800

    login

--- src/Login/login.js
+++ src/Login/login.js
@@ -276,7 +276,7 @@ export default class Login extends Component {
         const isMobile = (window.innerWidth < 768 || window.innerHeight < 768) ? true : false;
         const imgWidth = window.innerWidth < 1524 ? 1024 : window.innerWidth - 500;
 
-        const contentStyle = isMobile ? {} : { backgroundImage: 'url(sparks.jpg)', backgroundSize: (imgWidth) + 'px ' + window.innerHeight + 'px', opacity: 0.9 };
+        const contentStyle = isMobile ? {} : { backgroundImage: 'url(login/sparks.jpg)', backgroundSize: (imgWidth) + 'px ' + window.innerHeight + 'px', opacity: 0.9 };
         const loginBoxStyle = isMobile ? { width: '100%' } : { width: 500 };
         const loginTitleStyle = isMobile ? (window.innerHeight > 600 ? { marginTop: 0 - window.innerHeight * 0.1 } : { marginTop: 0 }) : { marginTop: 0 - window.innerHeight * 0.3 };
 
